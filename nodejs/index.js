14e8cf0512fc13a5c2d8c19e8eeccb27f31638dd
Marina Alane <marina.alane@outlook.com>
2021-08-25 10:19:45 -0300

    primeiros scripts

---
+++ nodejs/index.js
@@ -0,0 +1,24 @@
+
+// const readline = require('readline-sync');
+
+// const name = readline.question('Qual seu nome? ');
+// const age = readline.questionInt('Qual sua idade? ');
+
+// console.log(`Hello, ${name}! You are ${age} years old!`);
+
+// Função de soma.
+function soma(a,b) {
+  return 10 + 2
+ }
+ console.log(soma());
+
+ // calculo de IMC
+//  function imc(a,b) {
+
+//    const altura = 1.56
+//    const peso = 71.5
+
+//    return peso / (altura * altura)
+//  }
+
+//  console.log(imc());
